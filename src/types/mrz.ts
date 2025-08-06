6a7ad93603356d9207a12a1508c06697a8592393
Ruzmetov Nematjon <doonk.services@gmail.com>
2025-08-06 09:52:00 +0000

    Yine ayni hata
    eskiden asagidaki kodu kullanmisisiz.
    import {genkit} f

--- src/types/mrz.ts
+++ src/types/mrz.ts
@@ -9,7 +9,7 @@ export const ExtractMrzDataInputSchema = z.object({
   photoDataUri: z
     .string()
     .describe(
-      "A photo of a passport or ID card, as a data URI that must include a MIME type and use Base64 encoding. Expected format: 'data:<mimetype>;base64,<encoded_data>'."
+      "A photo of a passport or ID card, as a data URI that must include a MIME type and use Base64 encoding. Expected format: 'data:<mimetype>;base64,<encoded_data>'"
     ),
 });
 export type ExtractMrzDataInput = z.infer<typeof ExtractMrzDataInputSchema>;
